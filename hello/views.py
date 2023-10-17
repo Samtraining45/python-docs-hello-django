894fb8089d19ec99a78a5d997be533e6441d9cca
Samtraining45 <148282123+Samtraining45@users.noreply.github.com>
2023-10-18 02:20:35 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("********** -( Hi HPE Team good evening, My Name is 'AJAY', I am learning AZURE Cloud Computing[AZ900] ) **************** ")
+    return HttpResponse("********** -((👋 Hi, I’m @Samtraining45
+👀 I’m interested in learning new technologies
+🌱 I’m currently learning Azure
+💞️ I’m looking to collaborate on AZURE fundamentals
+📫 How to reach me +83763992)
